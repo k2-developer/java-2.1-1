194afd204fdab87408693e02bcfa4aefa4357233
Ivan Smetskii <smetskii.ivan@gmail.com>
2021-03-11 03:03:14 +0300

    123

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 5000;
+        int milesBonus = ticketCost / 20;
+
+        System.out.println("Сredited miles: " + milesBonus);
+    }
+}
